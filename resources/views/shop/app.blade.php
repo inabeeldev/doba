1479f83b9124f261287b30ec08868fa17e48a8a0
nabeeldevs <ahmad.nab331@gmail.com>
2024-05-11 02:57:19 +0500

    up

--- resources/views/shop/app.blade.php
+++ resources/views/shop/app.blade.php
@@ -147,6 +147,14 @@
       gtag('config', 'AW-16552512112');
     </script>
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N88HNGC2');</script>
+        <!-- End Google Tag Manager -->
+
 
 </head>
 
@@ -459,6 +467,11 @@
     </footer>
 
     <!-- Footer Section End -->
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N88HNGC2"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
 
    <!-- Js Plugins -->
     <script src="{{ asset('public/customer/js/jquery-3.3.1.min.js') }}"></script>
